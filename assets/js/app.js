0167d6f26e0597fddc871886327ff6ae96ff903f
Olof Olsson <borsna@gmail.com>
2016-03-06 15:49:36 +0100

    basic layout and game logic done
    need to make som things in the ui nicer

---
+++ assets/js/app.js
@@ -0,0 +1,56 @@
+﻿var correct = 0;
+var wrong = 0;
+
+function setState(state){
+	$('.game-state').hide();
+	$('.game-state-'+state).show();
+
+	if($( 'div[class^="mdl-layout__obfuscator"]' ).hasClass('is-visible')){
+		$( 'div[class^="mdl-layout__obfuscator"]' ).trigger( "click" );
+	}
+	
+	switch(state){
+		case 'game':
+			startGame();
+			break;
+	}
+}
+
+function guess(answer){
+	console.log('Guess: '+answer);
+	
+	if(quiz.current.f.toLowerCase().startsWith(answer)){
+		correct++;
+		$("#correct").fadeIn(100).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+	}else{
+		wrong++;
+		$("#wrong").fadeIn(100).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+	}
+	updateProgess();
+	
+	quiz.getNewSentence();
+	$('#sentence').html(quiz.current.s);	
+}
+
+function startGame(){
+	wrong = 0;
+	correct = 0;
+	updateProgess();
+	quiz.getNewSentence();
+	$('#sentence').html(quiz.current.s);
+}
+
+function updateProgess(){
+	$('#correct').html(correct);
+	$('#wrong').html(wrong);
+	var percent = correct/(correct+wrong)*100;
+	document.querySelector('#progress').MaterialProgress.setProgress(percent);
+}
+
+function reset(){
+	window.localStorage.removeItem("quiz-sentences");
+}
+
+String.prototype.startsWith = function(needle){
+    return(this.indexOf(needle) == 0);
+};
---
+++ assets/js/quiz.js
@@ -0,0 +1,146 @@
+var quiz = {
+    //Default settings
+	// http://spraakbanken.gu.se/ws/korp?command=info&corpus=
+	
+    backend : "http://spraakbanken.gu.se/ws/korp?",
+	familjeliv : [
+		"FAMILJELIV-ADOPTION",
+		"FAMILJELIV-ALLMANNA-EKONOMI",
+		"FAMILJELIV-ALLMANNA-FAMILJELIV",
+		"FAMILJELIV-ALLMANNA-FRITID",
+		"FAMILJELIV-ALLMANNA-HUSHEM",
+		"FAMILJELIV-ALLMANNA-HUSDJUR",
+		"FAMILJELIV-ALLMANNA-KROPP",
+		"FAMILJELIV-ALLMANNA-NOJE",
+		"FAMILJELIV-ALLMANNA-SAMHALLE",
+		"FAMILJELIV-ALLMANNA-SANDLADAN",
+		"FAMILJELIV-EXPERT",
+		"FAMILJELIV-FORALDER",
+		"FAMILJELIV-GRAVID",
+		"FAMILJELIV-KANSLIGA",
+		"FAMILJELIV-MEDLEM-ALLMANNA",
+		"FAMILJELIV-MEDLEM-FORALDRAR",
+		"FAMILJELIV-MEDLEM-PLANERARBARN",
+		"FAMILJELIV-MEDLEM-VANTARBARN",
+		"FAMILJELIV-PAPPAGRUPP",
+		"FAMILJELIV-PLANERARBARN",
+		"FAMILJELIV-SEXSAMLEVNAD",
+		"FAMILJELIV-SVARTATTFABARN",
+		"FAMILJELIV-ANGLARUM"],
+	flashback: [
+		"FLASHBACK-DATOR",
+		"FLASHBACK-DROGER",
+		"FLASHBACK-FORDON",
+		"FLASHBACK-HEM",
+		"FLASHBACK-KULTUR",
+		"FLASHBACK-LIVSSTIL",
+		"FLASHBACK-MAT",
+		"FLASHBACK-POLITIK",
+		"FLASHBACK-RESOR",
+		"FLASHBACK-SAMHALLE",
+		"FLASHBACK-SEX",
+		"FLASHBACK-SPORT",
+		"FLASHBACK-VETENSKAP",
+		"FLASHBACK-OVRIGT",
+		"FLASHBACK-FLASHBACK"	
+	],
+    params : {
+        command: 'query_sample',
+        corpus: 'FLASHBACK-POLITIK',
+        cqp: '[]',
+        start:0,
+        end:10,
+        defaultcontext:'1 sentence',
+        show_struct:['text_username', 'text_date', 'thread_title']
+    },
+    punct : ['.', ',', '!', '?', ';', '-', '"', '\'', '(', ')'],
+    current : {},
+    getNewSentence : function(){
+		if(window.localStorage['quiz-sentences'] == undefined){
+			window.localStorage['quiz-sentences'] = JSON.stringify(preloaded);
+		}
+	
+         var sentences = JSON.parse(window.localStorage['quiz-sentences']);
+         
+         var random = Math.floor(Math.random()*sentences.length);
+
+         quiz.current = sentences[random]
+         sentences.splice(random,1);
+         window.localStorage['quiz-sentences'] = JSON.stringify(sentences);
+
+         if(sentences.length < preloaded.length){
+             this.loadNewSentences();
+         }         
+         return quiz.current;
+    },
+    loadNewSentences : function(callback){
+         jQuery.ajaxSettings.traditional = true;
+
+         jQuery.ajax({
+				url:this.backend, 
+				dataType:'jsonp', data: {
+					'command':'info',
+					'corpus': this.getRandomCorpora().join(',')
+				}
+			}).done(function(data){
+				console.log(data);
+				$.each(data.corpora, function(index, corpus){
+					
+					var start = Math.floor(Math.random()*corpus.info.Sentences);
+					
+					jQuery.ajax({
+						url:quiz.backend, 
+						dataType:'jsonp', data: {
+							'command':'query',
+							'corpus': index,
+							'start': start,
+							'end': start,
+							'cqp': '[]',
+							'defaultcontext':'1 sentence',
+							'show_struct':['text_username', 'text_date', 'thread_title']							
+						}
+					}).done(function(data){
+						console.log(data);
+						
+						var sentence = "";
+						jQuery.each(data.kwic[0].tokens, function(key, val) {
+							if (jQuery.inArray(val.word, quiz.punct) > -1){ 
+								sentence = sentence.trim() + val.word;
+							}
+							else{
+								sentence += ' ' + val.word;
+							}
+						});
+						var sentences = JSON.parse(window.localStorage['quiz-sentences']);
+						sentences.push({s:sentence.trim(),f:data.kwic[0].corpus});						
+						
+						window.localStorage['quiz-sentences'] = JSON.stringify(sentences);
+					});
+					
+					
+					
+					
+				})
+			}
+		);
+    },
+	getRandomCorpora: function(){
+		var corpuslist = new Array();
+		
+		while(corpuslist.length < 5){
+			var corpus = this.familjeliv[Math.floor(Math.random()*this.familjeliv.length)];
+			if($.inArray(corpus, corpuslist) < 0){
+				corpuslist.push(corpus);
+			}
+		}
+		
+		while(corpuslist.length < 10){
+			var corpus = this.flashback[Math.floor(Math.random()*this.flashback.length)];
+			if($.inArray(corpus, corpuslist) < 0){
+				corpuslist.push(corpus);
+			}
+		}		
+		
+		return corpuslist;
+	}
+}
